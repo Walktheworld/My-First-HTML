001ab8bae8869f4a80e6231621fbdc9c01039898
Joe Brown <walktheworld@me.com>
2021-03-30 14:08:05 -0600

    just a test commit

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This is my first website, so why don't I introduce myself a little...";
 document.querySelector('body').appendChild(h2);
